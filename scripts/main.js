d8442060c01cc67ac6751e8626badb8f1b0d5b32
Thomas Cook <thomas.cook@lush.co.uk>
2023-07-04 10:02:32 +0100

    Undo code to make example for debugging simpler

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,1 @@
-static MODULE_ID = "traveller-tools";
-
-CONFIG.debug.hooks = true;
-
-console.log(MODULE_ID + ": Hello World!")
-
-static log(force, ...args) {
-    const shouldLog = force || game.modules.get("_dev-mode")?.api?.getPackageDebugValue(MODULE_ID);
-    if (shouldLog) {
-        console.log(MODULE_ID, "|", ...args);
-    }
-}
-
-Hooks.once("devModeReady", ({ registerPackageDebugFlag }) => {
-    registerPackageDebugFlag(MODULE_ID);
-});
+console.log("Hello World!")
